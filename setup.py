27df7e96d02cb88d5532d4b9b2927f9cdfee7212
Varuntejakulla <varuneeeaims@gmail.com>
2025-05-08 15:40:26 +0530

    src on setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="varun",
+    author_email="varuneeaims@gmail.com",
+    packages=find_packages()
+    
+)
